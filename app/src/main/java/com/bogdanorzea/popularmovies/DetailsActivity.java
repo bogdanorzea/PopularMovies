8264f71700080f5500a559d2fd154eb72ced568e
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-02-24 22:31:24 +0200

    Removes FAB, Adds rating, Adds Options menu

--- app/src/main/java/com/bogdanorzea/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/bogdanorzea/popularmovies/DetailsActivity.java
@@ -1,18 +1,22 @@
 package com.bogdanorzea.popularmovies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bogdanorzea.popularmovies.model.Movie;
 import com.bogdanorzea.popularmovies.utils.NetworkUtils;
@@ -25,32 +29,22 @@ import okhttp3.HttpUrl;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    ProgressBar progressBar;
-    private Movie mMovie;
-    private ConstraintLayout constraintLayout;
-    private AppBarLayout appBarLayout;
+    ProgressBar mProgressBar;
+    private Movie mCurrentMovie;
+    private ConstraintLayout mConstraintLayout;
+    private AppBarLayout mAppBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        progressBar = findViewById(R.id.progressBar);
-        constraintLayout = findViewById(R.id.constraint_layout);
-        appBarLayout = findViewById(R.id.app_bar);
-        //toolbar.setTitle("");
         setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mProgressBar = findViewById(R.id.progressBar);
+        mConstraintLayout = findViewById(R.id.constraint_layout);
+        mAppBarLayout = findViewById(R.id.app_bar);
 
         Intent intent = getIntent();
         // TODO add checks fot intent validity
@@ -64,29 +58,58 @@ public class DetailsActivity extends AppCompatActivity {
         }
     }
 
-    private void showProgress(){
-        progressBar.setVisibility(View.VISIBLE);
-        constraintLayout.setVisibility(View.INVISIBLE);
-        appBarLayout.setExpanded(false);
+    private void showProgress() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mConstraintLayout.setVisibility(View.INVISIBLE);
+        mAppBarLayout.setExpanded(false);
     }
 
-    private void hideProgress(){
-        appBarLayout.setExpanded(true);
-        progressBar.setVisibility(View.INVISIBLE);
-        constraintLayout.setVisibility(View.VISIBLE);
+    private void hideProgress() {
+        mAppBarLayout.setExpanded(true);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mConstraintLayout.setVisibility(View.VISIBLE);
     }
 
-    private void displayMovie() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_details, menu);
+        return true;
+    }
 
-        ((TextView) findViewById(R.id.movie_title)).setText(mMovie.title);
-        ((TextView) findViewById(R.id.movie_tagline)).setText(mMovie.tagline);
-        ((TextView) findViewById(R.id.movie_website)).setText(mMovie.homepage);
-        ((TextView) findViewById(R.id.movie_release_date)).setText(mMovie.releaseDate);
-        ((TextView) findViewById(R.id.movie_overview)).setText(mMovie.overview);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_website:
+                openMovieWebsite();
+                return true;
+            case R.id.action_trailer:
+                openMovieTrailer();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-        ImageView poster = findViewById(R.id.movie_poster);
+    private void openMovieTrailer() {
+        Toast.makeText(this, "Will be added soon", Toast.LENGTH_SHORT).show();
+    }
 
-        NetworkUtils.loadImage(this, poster, mMovie.backdropPath);
+    private void openMovieWebsite() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mCurrentMovie.homepage));
+        startActivity(intent);
+    }
+
+    private void renderMovieInformation() {
+        ImageView poster = findViewById(R.id.movie_backdrop);
+        NetworkUtils.loadImage(this, poster, mCurrentMovie.backdropPath);
+
+        ((TextView) findViewById(R.id.movie_title)).setText(mCurrentMovie.title);
+        ((TextView) findViewById(R.id.movie_tagline)).setText(mCurrentMovie.tagline);
+        ((TextView) findViewById(R.id.movie_website)).setText(mCurrentMovie.homepage);
+        ((TextView) findViewById(R.id.movie_release_date)).setText(mCurrentMovie.releaseDate);
+        ((TextView) findViewById(R.id.movie_overview)).setText(mCurrentMovie.overview);
+        ((RatingBar) findViewById(R.id.movie_score)).setRating((float) (mCurrentMovie.voteAverage / 2));
     }
 
     private class AT extends AsyncTask<HttpUrl, Void, Movie> {
@@ -121,9 +144,9 @@ public class DetailsActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Movie movie) {
-            mMovie = movie;
+            mCurrentMovie = movie;
             hideProgress();
-            displayMovie();
+            renderMovieInformation();
         }
     }
 }
