a6d4555abf52b493b708f91cc82c67a3c43bf494
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-02-25 21:08:35 +0200

    Add OptionsMenu to MainActivity

--- app/src/main/java/com/bogdanorzea/popularmovies/MainActivity.java
+++ app/src/main/java/com/bogdanorzea/popularmovies/MainActivity.java
@@ -1,10 +1,15 @@
 package com.bogdanorzea.popularmovies;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -54,6 +59,36 @@ public class MainActivity extends AppCompatActivity {
             textView.setText(R.string.warning_no_internet);
             textView.setVisibility(View.VISIBLE);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sorting:
+                showSortingDialog();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
+    private void showSortingDialog() {
+        new AlertDialog.Builder(this)
+                .setSingleChoiceItems(new String[] {"ana", "are", "mere" }, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
     }
 
     private void loadData() {
