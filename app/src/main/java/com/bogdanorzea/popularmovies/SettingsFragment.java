9e1cbe05223494a1ab7c165c9b98a3fddaf1a48b
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-02-26 22:54:43 +0200

    Improve the SettingActivity by adding summary

--- app/src/main/java/com/bogdanorzea/popularmovies/SettingsFragment.java
+++ app/src/main/java/com/bogdanorzea/popularmovies/SettingsFragment.java
@@ -1,12 +1,61 @@
 package com.bogdanorzea.popularmovies;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int preferenceCount = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < preferenceCount; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            setSummary(preference);
+        }
+    }
+
+    private void setSummary(Preference preference) {
+        if (preference instanceof PreferenceGroup) {
+            PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+            for (int i = 0; i < preferenceGroup.getPreferenceCount(); i++) {
+                setSummary(preferenceGroup.getPreference(i));
+            }
+        }
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            preference.setSummary(listPreference.getEntry());
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String preferenceKey) {
+        Preference preference = findPreference(preferenceKey);
+        if (null != preference) {
+            setSummary(preference);
+        }
     }
 }
