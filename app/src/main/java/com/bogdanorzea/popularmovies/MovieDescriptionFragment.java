fc5af03ba499fe4786e58fef04eed1f70f72d392
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-03-08 21:23:02 +0200

    Working skeleton to support the tabbed layout for DetailsActivity

---
+++ app/src/main/java/com/bogdanorzea/popularmovies/MovieDescriptionFragment.java
@@ -0,0 +1,19 @@
+package com.bogdanorzea.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MovieDescriptionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.movie_description_layout, container, false);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/bogdanorzea/popularmovies/MovieReviewsFragment.java
@@ -0,0 +1,55 @@
+package com.bogdanorzea.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.bogdanorzea.popularmovies.model.response.ReviewsResponse;
+import com.bogdanorzea.popularmovies.utils.AsyncTaskUtils;
+import com.bogdanorzea.popularmovies.utils.NetworkUtils;
+
+
+public class MovieReviewsFragment extends Fragment {
+    private int movieId;
+
+    private View view;
+    private AsyncTaskUtils.AsyncTaskListener<ReviewsResponse> mMovieReviewsAsyncTaskListener =
+            new AsyncTaskUtils.AsyncTaskListener<ReviewsResponse>() {
+                @Override
+                public void onTaskStarting() {
+
+                }
+
+                @Override
+                public void onTaskComplete(ReviewsResponse result) {
+                    displayReviews(result);
+                }
+            };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.movie_reviews_layout, container, false);
+
+        Bundle arguments = getArguments();
+        movieId = arguments.getInt("movie_id");
+
+        new AsyncTaskUtils.MovieReviewsAsyncTask(mMovieReviewsAsyncTaskListener)
+                .execute(NetworkUtils.movieReviewsUrl(movieId, 1));
+
+        return view;
+    }
+
+    private void displayReviews(ReviewsResponse result) {
+        ListView reviewsListView = view.findViewById(R.id.list);
+        ViewCompat.setNestedScrollingEnabled(reviewsListView, true);
+
+        ReviewsAdapter reviewsAdapter = new ReviewsAdapter(getActivity(), result.results);
+
+        reviewsListView.setAdapter(reviewsAdapter);
+    }
+
+}
