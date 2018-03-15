f6c8c68777f315da403b51d3a207b3a4e5afc2ec
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-03-15 21:49:07 +0200

    Change movie path in ContentProvider

--- app/src/main/java/com/bogdanorzea/popularmovies/data/MoviesContract.java
+++ app/src/main/java/com/bogdanorzea/popularmovies/data/MoviesContract.java
@@ -7,7 +7,7 @@ import android.provider.BaseColumns;
 public class MoviesContract {
 
     public static final String CONTENT_AUTHORITY = "com.bogdanorzea.popularmovies";
-    public static final String PATH_MOVIE = "favorite";
+    public static final String PATH_MOVIE = "movie";
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MOVIE);
